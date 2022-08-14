d64aa0f8aa7c08a45fca6aece10f2898ce016129
Garrett Downs <garredow@outlook.com>
2022-08-14 18:03:27 -0400

    chore: add manifests for kaios 3

--- deploy/version.sh
+++ deploy/version.sh
@@ -5,6 +5,8 @@ echo "Applying version ${VERSION} to manifests"
 cd public
 
 jq ".version = \"${VERSION}\"" manifest.webapp > tmp.json && mv tmp.json manifest.webapp
+jq ".b2g_features.version = \"${VERSION}\"" manifest.webmanifest > tmp.json && mv tmp.json manifest.webmanifest
+jq ".b2g_features.version = \"${VERSION}\"" manifest.en-US.webmanifest > tmp.json && mv tmp.json manifest.en-US.webmanifest
 
 cd -
 
